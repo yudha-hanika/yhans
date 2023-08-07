da2d9290e8e1575a3f82d49067762684780647f6
yudha-hanika <yhanika58@gmail.com>
2023-08-07 12:55:01 +0700

    perbaikan

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,10 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navList.classList.toggle('hidden');
 });
+
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
